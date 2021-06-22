fd60a6b77daf986af248173d846a90bbd541044d
Swapnil khare <swapnilk@neuronsolutions.com>
2021-06-22 12:21:57 +0530

    Update routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,7 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/task/create', 'TodolistController@create')->name('task.create');
+Route::get('/task-create', 'TodolistController@create')->name('task.create');
 Route::post('/task-store', 'TodolistController@store')->name('task.store');
 Route::get('/task/{id}/edit', 'TodolistController@edit')->name('task.edit');
 Route::post('/task-update', 'TodolistController@update')->name('task.update');
